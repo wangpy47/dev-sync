9251f36d2481efb98f3ef6c93c8da692c6794b0a
jihwankim97 <cool102476@naver.com>
2024-10-18 22:29:11 +0900

    feat(google-auth-): 구글 로그인 백엔드 프론트엔드 연동

--- back-end/src/auth/auth.controller.ts
+++ back-end/src/auth/auth.controller.ts
@@ -1,80 +1,21 @@
-import {
-  Body,
-  Controller,
-  Post,
-  Get,
-  Request,
-  Response,
-  UseGuards,
-} from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { CreateUserDto } from 'src/user/user.dto';
-import {
-  LoginGuard,
-  AuthenticatedGuard,
-  LocalAuthGuard,
-  GoogleAuthGuard,
-} from './auth.guard';
+import { Controller, Get, Request, Response, UseGuards } from '@nestjs/common';
+
+import { GoogleAuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor() {}
 
-  @Post('register')
-  async register(@Body() userDto: CreateUserDto) {
-    return await this.authService.register(userDto);
-  }
-
-  @Post('login')
-  async login(@Request() req, @Response() res) {
-    const userInfo = await this.authService.validateUser(
-      req.body.email,
-      req.body.password,
-    );
-
-    if (userInfo) {
-      res.cookie('login', JSON.stringify(userInfo), {
-        httpOnly: false,
-        maxAge: 1000 * 60 * 24 * 7,
-      });
+  @Get('status')
+  getAuthStatus(@Request() req) {
+    if (req.isAuthenticated()) {
+      // 세션이 유효하면 유저 정보 반환
+      return req.user;
+    } else {
+      // 세션이 유효하지 않으면 null 또는 에러 반환
+      return { message: 'Not authenticated' };
     }
-    return res.send({ message: 'login success' });
   }
-
-  @UseGuards(LoginGuard)
-  @Post('login2')
-  async login2(@Request() req, @Response() res) {
-    if (!req.cookies['login'] && req.user) {
-      res.cookie('login', JSON.stringify(req.user), {
-        httpOnly: true,
-
-        maxAge: 1000 * 10,
-      });
-    }
-    return res.send({ message: 'login2 success' });
-  }
-
-  @UseGuards(LoginGuard)
-  @Get('test-guard')
-  testGuard() {
-    return '로그인 완료';
-  }
-
-  @UseGuards(LocalAuthGuard)
-  @Post('login3')
-  login3(@Request() req) {
-    return req.user;
-  }
-
-  @UseGuards(AuthenticatedGuard)
-  @Get('test-guard2')
-  testGuardWithSession(@Request() req) {
-    return req.user;
-  }
-
-  @Get('to-google')
-  @UseGuards(GoogleAuthGuard)
-  async googleAuth(@Request() req) {}
 
   @Get('google')
   @UseGuards(GoogleAuthGuard)
@@ -83,7 +24,7 @@ export class AuthController {
     if (user) {
       // 로그인 성공 시 세션에 유저 정보 저장
       console.log('Google Auth successful:', user);
-      return res.redirect('/user/updateProfile'); // 원하는 경로로 리디렉션
+      return res.redirect('http://localhost:4000/'); // 원하는 경로로 리디렉션
     } else {
       console.log('Google Auth failed');
       return res.status(403).send('Authentication failed');
