ab1d8c6b3b31d8eaf26ff7650fac024cc8c9c28f
jihwankim97 <80530855+jihwankim97@users.noreply.github.com>
2024-12-21 23:05:26 +0900

    feat: contact 문의 기능 api 작성 contact-dto 생성 문의사항 메일 보내기 컨트롤러 작성 기능 테스트 완료 데이터베이스 저장 필요

--- back-end/src/contact/contact.controller.ts
+++ back-end/src/contact/contact.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ContactService } from './contact.service';
+import { ContactMailDto } from './contact.dto';
 
 @Controller('contact')
-export class ContactController {}
+export class ContactController {
+constructor(
+    private readonly contactService: ContactService
+  ) {}
+
+
+  @Post('contact-submit')
+  async submitInquiry(@Body() contact: ContactMailDto): Promise<string> {
+    console.log(contact)
+    return this.contactService.sendInquiryEmail(contact);
+  }
+
+}
--- back-end/src/contact/contact.service.ts
+++ back-end/src/contact/contact.service.ts
@@ -1,35 +1,40 @@
 import { Injectable } from '@nestjs/common';
-import { nodemailer } from 'nodemailer';
+import * as nodemailer from 'nodemailer';
+import { ContactMailDto } from './contact.dto';
 
 @Injectable()
 export class ContactService {
-  async sendInquiryEmail(inquiry) {
+  async sendInquiryEmail(inquiry: ContactMailDto): Promise<string> {
+    // SMTP Transporter 설정
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: process.env.ADMIN_EMAIL_JH,
-        pass: process.env.ADMIN_APP_PASS,
+        user: process.env.ADMIN_EMAIL_MK, 
+        pass: process.env.ADMIN_APP_PASS_MK, 
       },
     });
 
-    const mailOptions = {//내용
-      from: `"Contact Mannager System"<${process.env.ADMIN_EMAIL_JH}>`,
-      to: process.env.ADMIN_EMAIL_JH,
-      subject: `New Inquiry from ${inquiry.name}: ${inquiry.subject}`, // 제목
+    // 이메일 옵션
+    const mailOptions = {
+      from: `"Contact Manager System" <${process.env.ADMIN_EMAIL_MK}>`,
+      to: process.env.ADMIN_EMAIL_MK, 
+      subject: `New Inquiry from ${inquiry.name}: ${inquiry.subject}`, 
       text: `
         Name: ${inquiry.name}
         Email: ${inquiry.email}
         Subject: ${inquiry.subject}
         Message: ${inquiry.message}
-      `, 
+      `,
     };
+
     // 이메일 전송
     try {
       const info = await transporter.sendMail(mailOptions);
-      return `Email sent: ${info.response}`;
+      console.log('Email sent:', info.messageId);
+      return `Email sent successfully: ${info.messageId}`;
     } catch (error) {
-      console.error('Error sending email:', error);
-      throw new Error('Failed to send email');
+      console.error('Failed to send email:', error.message || error);
+      throw new Error(`Failed to send email: ${error.message}`);
     }
   }
 }
