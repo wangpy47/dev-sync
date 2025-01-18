92bfc271bb92073cee7e5b952a7b4f86af553685
jihwankim97 <cool102476@naver.com>
2025-01-18 23:13:53 +0900

    feat:카테고리 컨트롤러 작성 및, dto 작성

--- back-end/src/posts/posts.controller.ts
+++ back-end/src/posts/posts.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { GetPostsByCategoryDto } from './dto/category/get-posts-by-category.dto';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+
+    constructor(private readonly postsService:PostsService){}
+
+    //모든 카테고리 조회
+    @Get('/categories')
+    async getCateGories(){
+        return this.postsService.getCategories();
+    }
+
+
+    // 카테고리 이름으로 게시글 조회
+    @Get('/categories/posts')
+    async getPostsByCategory(@Query() getPostsByCategoryDto: GetPostsByCategoryDto) {
+        return await this.postsService.getPostsByCategory(getPostsByCategoryDto);
+    }
+
+}
