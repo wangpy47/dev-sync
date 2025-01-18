5936a36ae4c040ae977f631ceb78a075f39ebef2
jihwankim97 <cool102476@naver.com>
2025-01-18 23:26:50 +0900

    feat: category API URL및, Dto 개선

--- back-end/src/posts/posts.controller.ts
+++ back-end/src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { GetPostsByCategoryDto } from './dto/category/get-posts-by-category.dto';
 
@@ -15,9 +15,9 @@ export class PostsController {
 
 
     // 카테고리 이름으로 게시글 조회
-    @Get('/categories/posts')
-    async getPostsByCategory(@Query() getPostsByCategoryDto: GetPostsByCategoryDto) {
-        return await this.postsService.getPostsByCategory(getPostsByCategoryDto);
+    @Get('/:name/posts') 
+    async getPostsByCategory(@Param() params: GetPostsByCategoryDto) {
+        return await this.postsService.getPostsByCategory(params);
     }
 
 }
